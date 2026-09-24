937fbe16f5e50b9beeb4d0910c6a96314f897aeb
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-24 13:27:57 -0300

    feat: carregar módulo de doações

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -27,3 +27,4 @@ require_once __DIR__ . '/csrf.php';
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/pix.php';
 require_once __DIR__ . '/raffle.php';
+require_once __DIR__ . '/donations.php';
